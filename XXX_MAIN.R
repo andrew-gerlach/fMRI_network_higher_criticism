838ac9715a39436132a09902bfd17d090f8b198b
Andrew Gerlach <gerlach.andrew@gmail.com>
2025-09-25 17:04:42 -0400

    Initial commit with some shell scripts

---
+++ XXX_MAIN.R
@@ -0,0 +1,17 @@
+#' Primary routine
+#'
+#' This is the primary routine to drive network inference on functional
+#' connectomes using higher criticism
+
+# Step 1 convert test information to a formula
+
+
+
+# Step 2 run first level tests (call to another routine)
+# mclapply(RUN_1ST_LEVEL_TESTS, <inputs for RUN_1ST_LEVEL_TESTS>, <options for mclapply>)
+
+
+
+# Step 3 calculate network level HC statistics (call to another routine)
+# RUN_2ND_LEVEL_TESTS()
+# p is main input, don't set alpha, set k1=0.5, emp=F

1d2d993bb1e43ccb67ea9b2c05579ee25eb22bd6
Andrew Gerlach <gerlach.andrew@gmail.com>
2025-10-23 13:38:02 -0400

    ARoG: added some error checking to 1st level test script

--- XXX_RUN_1ST_LEVEL_TESTS.R
+++ XXX_RUN_1ST_LEVEL_TESTS.R
@@ -13,6 +13,16 @@ XXX_RUN_1ST_LEVEL_TESTS = function(data, fc){
   library(parallel)
   library(dplyr)
   
+  # Number of subjects
+  n = nrow(data)
+  
+  # Number of nodes in FC matrices
+  k = dim(fc)[2]
+  
+  # Error checking
+  if(dim(fc)[1] != n) { stop(sprintf("%i subjects in data, but %i FC matrices provided!", n, dim(fc)[1])) }
+  if(dim(fc)[3] != k) { stop("FC matrices provided are not square!") }
+  
   # Boolean array for upper triangular matrix
   upper = upper.tri(diag(dim(fc)[2]), diag = FALSE)
   # Number of unique entries in upper triangular matrix

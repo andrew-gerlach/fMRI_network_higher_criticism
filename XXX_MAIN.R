4fa18a51067084fcf8a640a186e9f71aa3b81ee8
Andrew Gerlach <gerlach.andrew@gmail.com>
2025-12-03 16:03:49 -0500

    Implemented basic function for reading FC matrices from csv, R, or matlab files Added option to read data from file as well

--- XXX_MAIN.R
+++ XXX_MAIN.R
@@ -6,10 +6,13 @@
 #' @param data data frame containing subject level variables (n rows)
 #' @param fc functional connectivity matrices (3D array, n x k x k)
 #' @param fc_col_name column name in data with subject level FC files (string)
+#' @param fc_obj_name name of FC matrix object in storage structure (string)
 #' @param alpha HC control parameter (optional, numeric in (0, 1))
 #' @param k1 HC control parameter (optional, numeric in (0, 1))
 #' @param emp HC control parameter (optional, boolean)
 #' @param plot flag to return HC plots (optional, boolean)
+
+# packages: tidyverse, tools, readxl, R.matlab
 
 ### Step 0 process input
 
@@ -23,10 +26,31 @@ hc_opts = list(alpha = alpha,
                emp = emp,
                plot = plot)
 
+# Read in data file if needed
+if(!is.data.frame(data)) {
+  if(is.character(data)) {
+    if(file.exists(data)) {
+      file_type = tolower(file_ext(data))
+      if(file_type == "csv") {
+        data = read.csv(data)
+      } else if(file_type == ".xlsx" | file_type == ".xls") {
+        data = read_excel(data)
+      } else {
+        stop("Data file of unrecognized type, please provide .csv or Excel filetype")
+      }
+    } else {
+      stop("Data file does not seem to exist!")
+    }
+  } else {
+    stop("Unrecognized format of data! Please supply a data frame/tibble or readable filename (full path)")
+  }
+}
+
 ### Step 1a convert test information to a formula
 
 ### Step 1b load fc data into array if needed
-if(is.null(fc)) { fc = XXX_RETRIEVE_FC_MATRICES(data, fc_col_name) }
+if(is.missing(fc_obj_name)) {fc_obj_name = NULL }
+if(is.null(fc)) { fc = XXX_RETRIEVE_FC_MATRICES(data, fc_col_name, fc_obj_name) }
 
 ### Step 2 run first level tests (call to another routine)
 # mclapply(RUN_1ST_LEVEL_TESTS, <inputs for RUN_1ST_LEVEL_TESTS>, <options for mclapply>)
--- XXX_RETRIEVE_FC_MATRICES.R
+++ XXX_RETRIEVE_FC_MATRICES.R
@@ -2,12 +2,30 @@
 #'
 #' @param data data frame containing subject level variables (n rows)
 #' @param fc_col_name column name in data with subject level FC files (string)
+#' @param fc_obj_name name of FC matrix object in storage structure (string)
 
-XXX_RETRIEVE_FC_MATRICES = function(data, fc_col_name) {
+XXX_RETRIEVE_FC_MATRICES = function(data, fc_col_name, fc_obj_name) {
   
+  # number of subjects
   n = nrow(data)
   
-  fc_mat = # figure out how to read in the first FC matrix
+  # read in first matrix for data gathering
+  fc_fn = data[, fc_col_name] %>% first(na_rm=T)
+  if(file.exists(fc_fn)) {
+    fc_fn_type = tolower(file_ext(fc_fn))
+    if(fc_fn_type == "csv") {
+      fc_mat = read.csv(fc_fn)
+    } else if(fc_fn_type == "rds") {
+      fc_mat = readRDS(fc_fn)
+    } else if(fc_fn_type == "rdata" | fc_fn_type == "rda") {
+      fc_mat = load(fc_fun)[[fc_obj_name]]
+    } else if(fc_fn_type == "mat") {
+      fc_mat = readMat(fc_fn)[[fc_obj_name]]
+    } else {
+      # TODO: Add support for Python
+      stop("The FC file type is not supported, please provide a .csv, .rds, .rdata, .rda, or .mat file")
+    }
+  }
   
   # extract size and basic checks
   if(length(dim(fc_mat)) != 2) { stop("The FC matrix is not 2D!") }
@@ -17,20 +35,42 @@ XXX_RETRIEVE_FC_MATRICES = function(data, fc_col_name) {
   # initialize FC storage array
   fc = array(NA, c(n, k, k))
   fc[1, , ] = fc_mat
+  rm(fc_mat)
   
   for(subj in 2 : n) {
     
+    fc_fn = data[subj, fc_col_name]
+    
     # check if file exists
-    if(!file.exists(data[fc_col_name, subj])) {
-      warning(sprintf("FC matrix not found: %s", data[fc_col_name, subj]))
+    if(!file.exists(fc_fn)) {
+      warning(sprintf("FC matrix not found: %s", fc_fn))
       next
     }
     
-    fc_mat = # figure out how to read in the FC matrix
+    # read in FC file
+    if(fc_fn_type == "csv") {
+      fc_mat = read.csv(fc_fn)
+    } else if(fc_fn_type == "rds") {
+      fc_mat = readRDS(fc_fn)
+    } else if(fc_fn_type == "rdata" | fc_fn_type == "rda") {
+      if(missing(fc_obj_name) | is.null(fc_obj_name)) {
+        stop("The FC matrix object (variable) name is required for .rdata/.rda files!")
+      }
+      fc_mat = load(fc_fun)[[fc_obj_name]]
+    } else if(fc_fn_type == "mat") {
+      if(missing(fc_obj_name) | is.null(fc_obj_name)) {
+        stop("The FC matrix object (variable) name is required for .mat files!")
+      }
+      fc_mat = readMat(fc_fn)[[fc_obj_name]]
+    } else {
+      # TODO: Add support for Python
+      stop("The FC file type is not supported, please provide a .csv, .rds, .rdata, .rda, or .mat file")
+    }
     fc[subj, , ] = fc_mat
+    rm(fc_mat)
     
   }
   
-  
+  return(fc)
   
 }

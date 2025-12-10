9b19a3eeef2ec6e79255cceaea8fd3d263e4c9e8
Andrew Gerlach <gerlach.andrew@gmail.com>
2025-12-10 14:18:50 -0500

    Added support for reading network definition from file and low/high testing

--- XXX_MAIN.R
+++ XXX_MAIN.R
@@ -3,18 +3,20 @@
 #' This is the primary routine to drive network inference on functional
 #' connectomes using higher criticism
 #'
-#' @param data data frame containing subject level variables (n rows)
+#' @param data data table containing subject level variables (string, data.frame, n rows)
 #' @param test_type description of statistical test type (string)
-#' @param from formula for statistical test (string, formula)
-#' @param fc functional connectivity matrices (3D array, n x k x k)
-#' @param fc_col_name column name in data with subject level FC files (string)
-#' @param fc_obj_name name of FC matrix object in storage structure (string)
+#' @param form formula for statistical test (string, formula)
+#' @param net_def network definition for nodes (string, data.frame)
+#' @param net_def_col name of column containing network definition (optional, string)
+#' @param fc functional connectivity matrices (optional, 3D array, n x k x k)
+#' @param fc_col_name column name in data with subject level FC files (optional, string)
+#' @param fc_obj_name name of FC matrix object in storage structure (optional, string)
 #' @param alpha HC control parameter (optional, numeric in (0, 1))
 #' @param k1 HC control parameter (optional, numeric in (0, 1))
 #' @param emp HC control parameter (optional, boolean)
 #' @param plot flag to return HC plots (optional, boolean)
 
-XXX_MAIN = function(data, test_type, form, fc, fc_col_name, fc_obj_name, alpha, k1, emp, plot) {
+XXX_MAIN = function(data, test_type, form, net_def, net_def_col, fc, fc_col_name, fc_obj_name, alpha, k1, emp, plot) {
 
 # packages: tidyverse, tools, readxl, R.matlab
 require(tidyverse)
@@ -55,11 +57,47 @@ if(!is.data.frame(data)) {
   }
 }
 
-### Step 1a convert test information to a formula
-
-### Step 1b load fc data into array if needed
+### Step 1a load fc data into array if needed
 if(missing(fc_obj_name)) {fc_obj_name = NULL }
-if(missing(fc)) { fc = XXX_RETRIEVE_FC_MATRICES(data, fc_col_name, fc_obj_name) }
+if(missing(fc)) { 
+  if(missing(fc_col_name)) {
+    stop("Must provide FC array or column name with paths to FC matrix files")
+  } else {
+    fc = XXX_RETRIEVE_FC_MATRICES(data, fc_col_name, fc_obj_name)
+  }
+}
+
+### Step 1b load network definitions if needed
+if(!is.vector(net_def) | length(net_def) == 1) {
+  
+  # read in file name is provided
+  if(is.character(net_def)){
+    if(file.exists(net_def)) {
+      file_type = tolower(file_ext(net_def))
+      if(file_type == "csv") {
+        net_def = read.csv(net_def)
+      } else if(file_type == ".xlsx" | file_type == ".xls") {
+        net_def = read_excel(net_def)
+      } else {
+        stop("Network definition file of unrecognized type, please provide .csv or Excel filetype")
+      }
+    } else {
+      stop("Network definition file does not seem to exist!")
+    }
+  }
+  
+  # pull network definition column if needed
+  if(is.data.frame(net_def)) {
+    if(missing(net_def_col)) {
+      stop("Network definition column name needed to read in network definition from table")
+    } else {
+      net_def = net_def %>% pull(net_def_col)
+    }
+  } else {
+    stop("Unrecognized format of network definition! Please supply a data frame/tibble or readable filename (full path)")
+  }
+  
+}
 
 ### Step 2 run first level tests (call to another routine)
 # mclapply(RUN_1ST_LEVEL_TESTS, <inputs for RUN_1ST_LEVEL_TESTS>, <options for mclapply>)
@@ -74,6 +112,7 @@ tmp = XXX_RUN_2ND_LEVEL_TESTS(first_level_results, net_def, hc_opts)
 second_level_results = tmp$second_level_results
 hc_plots = tmp$hc_plots
 
-return(second_level_results)
+return(list(second_level_results=second_level_results,
+            hc_plot=hc_plots))
 
 }

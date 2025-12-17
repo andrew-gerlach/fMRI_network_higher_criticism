e5fd7ae4390bc5c13d594a70b2e7bc76fda85a05
Andrew Gerlach <arg151@pitt.edu>
2025-12-16 19:59:01 -0500

    updating specific data generation scripts with new name

--- testing/gen_50node_3net_ttest_data.R
+++ testing/gen_50node_3net_ttest_data.R
@@ -1,4 +1,5 @@
-tmp = XXX_generate_test_data(100, 50, net_def, c(0.2, 0.2, 0.5, 0, 0, 0), c(0.1, 0.3, 0.1, 0, 0, 0), 123)
+# Note, run this from the testing folder
+tmp = fCOuNT_generate_test_data(100, 50, net_def, c(0.2, 0.2, 0.5, 0, 0, 0), c(0.1, 0.3, 0.1, 0, 0, 0), 123)
 tmp$net_def = c(rep("A", 25), rep("B", 15), rep("C", 10))
 
-saveRDS(tmp, "~/Fellowship/projects/hc/fMRI_network_higher_criticism/testing/testdata_50node_3net_ttwo.RDS")
+saveRDS(tmp, "testdata_50node_3net_ttwo.RDS")
--- testing/gen_50node_3net_ttest_data.m
+++ testing/gen_50node_3net_ttest_data.m
@@ -1,2 +1,3 @@
+% Note, run this from the testing folder
 net_def = categorical([repmat("A", 1, 25), repmat("B", 1, 15), repmat("C", 1, 10) ]);
-[~, ~] = XXX_generate_test_data(100, 50, net_def, [0.3, 0.2, 0.5, 0, 0, 0], [0.1, 0.3, 0.1, 0, 0, 0], "ttwo", 311, "50node_3net");
+[~, ~] = fCOuNT_generate_test_data(100, 50, net_def, [0.3, 0.2, 0.5, 0, 0, 0], [0.1, 0.3, 0.1, 0, 0, 0], "ttwo", 311, "50node_3net");

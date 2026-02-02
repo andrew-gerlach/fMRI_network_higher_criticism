d456ad4012a768fa227878532f7479bc59349daf
Andrew Gerlach <gerlach.andrew@gmail.com>
2026-02-02 15:36:04 -0500

    Added purple for mixed results to plot results routine

--- fCOuNT_PLOT_RESULTS.R
+++ fCOuNT_PLOT_RESULTS.R
@@ -7,7 +7,7 @@
 #' @param font_size font size for network labels on plot
 #' @param label_height height of label track on plot
 #'
-#' @returns 
+#' @returns
 #' @export results_plot circle plot of network
 #'
 #' @examples
@@ -17,19 +17,19 @@ fCOuNT_PLOT_RESULTS = function(second_level_results, net_def, mcc, font_size, la
   # set graphic defaults
   if(missing(font_size)) { font_size = 1 }
   if(missing(label_height)) { label_height = 10 }
-  
+
   # pull network info from net_def
   networks = unique(net_def[!is.na(net_def)])
   m = length(networks)
   # number of unique network pairs
   M = m * (m + 1) / 2
-  
+
   # LLD vs Healthy comparisons circle plot
   plot_mat = matrix(NA, m, m)
   exit = FALSE
   j = 1
   while(!exit) {
-    
+
     # set significance threshold
     if(mcc == "none") {
       mcc_factor = 1 / 2
@@ -38,53 +38,61 @@ fCOuNT_PLOT_RESULTS = function(second_level_results, net_def, mcc, font_size, la
     }
     # track number of significant tests for FDR criterion
     j_old = j
-    
+
     for(net1 in 1:m) {
-      
+
       for(net2 in 1:m) {
-        
+
         # TODO: add checks for significant results in both directions
-        
+
         # Label low results with blue
         p = second_level_results %>%
           filter((network1 == networks[net1] & network2 == networks[net2]) |
                  (network1 == networks[net2] & network2 == networks[net1]),
                  direction == "low") %>%
           pull(p)
-        
+
         if(is.na(plot_mat[net1, net2]) & p < (0.05 * mcc_factor)) {
           plot_mat[net1, net2] = "blue"
           plot_mat[net2, net1] = "blue"
           j = j + 1
         }
-        
+
+        # Label high results with red
         p = second_level_results %>%
           filter((network1 == networks[net1] & network2 == networks[net2]) |
                  (network1 == networks[net2] & network2 == networks[net1]),
                  direction == "high") %>%
           pull(p)
-        
+
         if(is.na(plot_mat[net1, net2]) & p < (0.05 * mcc_factor)) {
           plot_mat[net1, net2] = "red"
           plot_mat[net2, net1] = "red"
           j = j + 1
         }
-        
+
+        # Label mixed results with purple
+        if(plot_mat[net1, net2] == "blue" & p < (0.05 * mcc_factor)) {
+          plot_mat[net1, net2] = "purple"
+          plot_mat[net2, net1] = "purple"
+          j = j + 1
+        }
+
       }
-      
+
     }
-    
+
     # FWE correction with Bonferroni
     if(mcc == "bonferroni" | mcc == "none") { exit = TRUE }
-    
+
     # FDR correction with Benjamini-Hochberg
     if(j == j_old) { exit = TRUE }
-    
+
   }
-  
+
   rownames(plot_mat) = networks
   colnames(plot_mat) = networks
-  
+
   plot_data = data.frame(from = rep(rownames(plot_mat), times = ncol(plot_mat)),
                          to = rep(colnames(plot_mat), each = nrow(plot_mat)),
                          color = as.vector(plot_mat),
@@ -108,9 +116,9 @@ fCOuNT_PLOT_RESULTS = function(second_level_results, net_def, mcc, font_size, la
                 niceFacing=T,
                 col="black",
                 cex=font_size)
-    
+
   }
-  
+
 }
 
 
